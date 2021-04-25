1a2e5a811f4409860f08c79a95818674229b8b5a
Leo <leosmerling-hopeit@noreply.github.com>
2021-04-25 23:31:35 +0200

    Samsa plugin push/consume initial naive version
    Consumer groups support. No locking
    
    Consumer groups support. No locking
    
    Added stats endpoint
    
    Updated scripts
    
    Client push example
    
    Get stats from client
    
    Example with 3 concurrent partitions
    
    Working distributed client
    
    Serialization / encoding on client side
    
    Refactored SamsaClient
    
    Samsa StreamManager implementation
    
    Simple example app working with Samsa streams plugin
    
    Implemented stream maxlen
    
    Removed size calculation
    
    in-process (no http) push and consume
    
    launch scripts examples
    
    Cluster stats endpoint
    
    Cluster demo. Cleanup
    
    Track producer and consumer ids
    
    Track producer and consumer ids
    
    Randomize load balancing
    
    Randomize load balancing
    
    Optimal cluster setup
    
    Renamed stats
    
    Stream recovery delay updated
    
    cluster setup

---
+++ plugins/streams/samsa/src/hopeit/samsa/cluster_stats.py
@@ -0,0 +1,27 @@
+from typing import Dict, List
+
+from hopeit.app.context import EventContext
+from hopeit.app.logger import app_extra_logger
+from hopeit.dataobjects import dataobject, dataclass
+
+from hopeit.samsa import Stats
+from hopeit.samsa.client import SamsaClient
+
+logger, extra = app_extra_logger()
+
+__steps__ = ['stats']
+
+
+@dataobject
+@dataclass
+class ClusterStats:
+    nodes: Dict[str, Stats]
+
+
+async def stats(payload: None, context: EventContext, nodes: str) -> ClusterStats:
+    client = SamsaClient(
+        push_nodes=[], 
+        consume_nodes=nodes.split(','), 
+        consumer_id=f"{context.app_key}.{context.event_name}"
+    )
+    return ClusterStats(nodes=await client.stats())
