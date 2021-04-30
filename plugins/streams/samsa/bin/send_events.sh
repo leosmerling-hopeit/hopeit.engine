f0284abb7c97e8d686756cf4d5c06607150048ed
Leo Smerling <leosmerling-hopeit@noreply.github.com>
2021-04-30 20:58:21 +0200

    Updated scripts

--- plugins/streams/samsa/bin/send_events.sh
+++ plugins/streams/samsa/bin/send_events.sh
@@ -4,7 +4,7 @@ i = "0"
 
 template='{"items": [{"key": "%s", "payload": "x"}]}'
 
-while [[ $i -le 100 ]]
+while [[ $i -le 1000 ]]
 do
     json_string=$(printf "$template" "$i")
     curl -i -d "$json_string" "localhost:8020/api/samsa/1x0/push?stream_name=test_stream"
