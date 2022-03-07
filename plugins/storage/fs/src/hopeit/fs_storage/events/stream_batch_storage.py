3f0dbca813740016504986ca4a64af16c1a213f7
Leo <leosmerling@noreply.github.com>
2022-03-07 18:16:36 +0100

    Avoiding buffer race conditions

--- plugins/storage/fs/src/hopeit/fs_storage/events/stream_batch_storage.py
+++ plugins/storage/fs/src/hopeit/fs_storage/events/stream_batch_storage.py
@@ -3,7 +3,7 @@ import os
 import uuid
 from collections import defaultdict
 from dataclasses import dataclass, field
-from datetime import datetime
+from datetime import datetime, timezone
 from pathlib import Path
 from typing import Dict, List, Optional
 
@@ -26,6 +26,7 @@ class Partition:
     items: List[DataObject] = field(default_factory=list)
 
 buffer: Dict[str, Partition] = {}
+buffer_lock: asyncio.Lock = asyncio.Lock()
 
 
 @dataobject
@@ -36,33 +37,50 @@ class FlushSignal:
 
 async def __service__(context: EventContext) -> Spawn[FlushSignal]:
     global buffer
-    while True:
-        await asyncio.sleep(60.0)
-        for partition_key in list(buffer.keys()):
-            yield FlushSignal(partition_key=partition_key)
+    settings: FileStorageSettings = context.settings(datatype=FileStorageSettings)
+    if settings.flush_seconds:
+        while True:
+            await asyncio.sleep(settings.flush_seconds)
+            for partition_key in list(buffer.keys()):
+                yield FlushSignal(partition_key=partition_key)
+    else:
+        if settings.flush_max_size == 0:
+            logger.warning(
+                context, 
+                "Flushing partitions by size and time are disabled."
+                "Specify either `flush_seconds` or `flush_max_size`"
+                "to enable flushing the buffer periodically"
+            )
+        else:
+            logger.info(context, "Flushing partitions by time disabled.")
 
 
 async def buffer_item(payload: DataObject, context: EventContext) -> Optional[FlushSignal]:
-    global buffer
-    ts = payload.event_ts() or datetime.now()
-    partition_key = ts.strftime("%Y/%m/%d/")
-    partition = buffer.get(partition_key, Partition())
+    global buffer, buffer_lock
+    settings: FileStorageSettings = context.settings(datatype=FileStorageSettings)
+    ts = payload.event_ts() or datetime.now(tz=timezone.utc)
+    partition_key = ts.strftime(
+        (settings.partition_dateformat.strip('/') + '/') or "%Y/%m/%d/"
+    )
+    async with buffer_lock:
+        partition = buffer.get(partition_key, Partition())
+        buffer[partition_key] = partition
     async with partition.lock:
         partition.items.append(payload)
-    buffer[partition_key] = partition
-    if len(partition.items) >= 10:
+    if settings.flush_max_size and len(partition.items) >= settings.flush_max_size:
         return FlushSignal(partition_key=partition_key)
     return None
 
 
 async def flush(signal: FlushSignal, context: EventContext):
-    global buffer
+    global buffer, buffer_lock
     logger.info(context, f"Flushing partition {signal.partition_key}...")
     partition = buffer[signal.partition_key]
     async with partition.lock:
         if len(partition.items):
             await _save_partition(signal.partition_key, partition.items, context)
-        del buffer[signal.partition_key]
+        async with buffer_lock:
+            del buffer[signal.partition_key]
     logger.info(context, f"Flush {signal.partition_key} done.")
 
 
