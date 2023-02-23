3c1fea5ede251db89e89eeaf615a78ade48845b5
Leo <leosmerling@noreply.github.com>
2023-02-23 18:59:49 +0100

    Locust basic loadtest

--- apps/loadtest/locust.py
+++ apps/loadtest/locust.py
@@ -3,17 +3,17 @@ import logging
 import json
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 2)
+    # wait_time = between(1, 2)
 
     def __init__(self, parent):
         super(QuickstartUser, self).__init__(parent)
         logging.info("Logging in...")
         self.token = ""
         self.headers = {}
-        self.client.headers.update({"Authorization":"Basic cndlcjp3ZXJ3ZXI="})
-        self.client.headers.update({"Accept":"application/json"})
-        self.client.headers.update({"X-Track-Caller":"4324"})
-        self.client.headers.update({"x-track-Session-id":"4324"})
+        self.client.headers.update({"Authorization": "Basic cndlcjp3ZXJ3ZXI="})
+        self.client.headers.update({"Accept": "application/json"})
+        self.client.headers.update({"X-Track-Caller": "4324"})
+        self.client.headers.update({"x-track-Session-id": "4324"})
 
         # response = self.client.get("/api/simple-example/0x20/basic-auth/0x20/login")
         # print(json.loads(response._content))
@@ -23,8 +23,10 @@ class QuickstartUser(HttpUser):
 
     def login(self):
           response = self.client.get("/api/simple-example/0x20/basic-auth/0x20/login")
-          return json.loads(response._content)['access_token']
+          ret = json.loads(response._content)
+          print(ret)
+          return ret['access_token']
 
-    @task(1)
+    @task
     def hello_query_something(self):
-        self.client.get("/api/simple-example/0x20/query-something?item_id=p_id")
+        self.client.get("/api/simple-example/0x20/query-something?item_id=loadtest&partition_key=2023%2F02%2F23%2F17")
